26088a6d8dafc7aa34c9bcfc4c6fa3aaf10e6e13
Matheus-Lenzi <matheus_lenzi10@hotmail.com>
2021-02-28 20:41:07 -0300

    Create result's template, change detail's template and implement generic views

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,17 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    #/polls/
-    path('', views.index, name='index'),
-    #/polls/question_id
-    path('<int:question_id>/', views.detail, name='detail'),
-    #/polls/question_id/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    #/polls/question_id/results/vote/
+    # #/polls/
+    # path('', views.index, name='index'),
+    # #/polls/question_id
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # #/polls/question_id/results/
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # #/polls/question_id/results/vote/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
