0ee6474f54b993e8a0d2276a9885b408028c2da0
Matheus Lenzi dos Santos <matheus_lenzi10@hotmail.com>
2021-03-18 23:02:03 -0300

    Revert "Implement the static files and start to styling with css"
    This reverts commit f00ea66919a3e946468adabcbbe83ec1cb15b47a.

--- polls/views.py
+++ polls/views.py
@@ -25,7 +25,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     def get_queryset(self):
         # return Question.objects.order_by('-pub_date')[:5]
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date=timezone.now()).order_by('-pub_date')[:5]
 
 # def detail(request, question_id):
 #     # try:
@@ -40,7 +40,7 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(pub_date=timezone.now())
 
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
